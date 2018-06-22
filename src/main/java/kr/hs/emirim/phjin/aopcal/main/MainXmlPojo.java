023abb72561201809d18ecdd1d267a76902eddce
Heejin Park <phjin1106@gmail.com>
2018-06-22 09:07:29 +0900

    XML 스키마 기반으로 AOP 구현

---
+++ src/main/java/kr/hs/emirim/phjin/aopcal/main/MainXmlPojo.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.phjin.aopcal.main;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.hs.emirim.phjin.aopcal.cal.Calculator;
+
+public class MainXmlPojo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:aopPogo.xml");
+		Calculator impcal = ctx.getBean("impCal", Calculator.class);
+		long fiveFact1 = impcal.factorial(5);
+		System.out.println("impCal.factorial(5) = "+fiveFact1);
+		
+		Calculator recCal = ctx.getBean("recCal", Calculator.class);
+		long fiveFact2 = recCal.factorial(5);
+		System.out.println("impCal.factorial(5) = "+fiveFact2);
+	}
+
+}
